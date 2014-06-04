96d544b09536bb34811d6551f235548d2a7c0b5f
antonpolishko <anton.polishko@gmail.com>
2014-06-03 19:27:15 -0700

    Updated the naming scheme for folders

--- img2folder.sh
+++ img2folder.sh
@@ -14,21 +14,21 @@ if [ ! -d "${path}/${Year}" ];
     echo "Create ${path}/${Year}"
     mkdir ${path}/${Year}
 fi
-if [ ! -d "${path}/${Year}/${Month}" ];
+if [ ! -d "${path}/${Year}/${Year}-${Month}" ];
     then
-    echo "Create ${path}/${Year}/${Month}"
+    echo "Create ${path}/${Year}/${Year}-${Month}"
     mkdir ${path}/${Year}/${Month}
 fi
-if [ ! -d "${path}/${Year}/${Month}/${Day}" ];
+if [ ! -d "${path}/${Year}/${Year}-${Month}/${Year}-${Month}-${Day}" ];
     then
-    echo "Create ${path}/${Year}/${Month}/${Day}"
-    mkdir "${path}/${Year}/${Month}/${Day}"
+    echo "Create ${path}/${Year}/${Year}-${Month}/${Year}-${Month}-${Day}"
+    mkdir "${path}/${Year}/${Year}-${Month}/${Year}-${Month}-${Day}"
 fi
 fileName=$(basename ${file})
 count=1
-if [ -e ${path}/${Year}/${Month}/${Day}/${fileName} ]
+if [ -e ${path}/${Year}/${Year}-${Month}/${Year}-${Month}-${Day}/${fileName} ]
 then
-    while [ -e ${path}/${Year}/${Month}/${Day}/${fileName}_${count} ]
+    while [ -e ${path}/${Year}/${Year}-${Month}/${Year}-${Month}-${Day}/${fileName}_${count} ]
     do
         echo $count
         let count+=1
@@ -37,4 +37,4 @@ then
 
 fi
 #echo "resulting name ${path}/${Year}/${Month}/${Day}/${fileName}"
-cp ${file} ${path}/${Year}/${Month}/${Day}/${fileName}
+cp ${file} ${path}/${Year}/${Year}-${Month}/${Year}-${Month}-${Day}/${fileName}
